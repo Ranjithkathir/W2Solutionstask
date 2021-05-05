b3911d2d82b190785562507a6d793247ce0620cb
Ranjithkathir <40994267+Ranjithkathir@users.noreply.github.com>
2021-05-05 19:07:45 +0530

    Revert "CRUD & Multiauth menus based on role"
    This reverts commit 114ce85d1bc1ffaa42f89ef1647013c0aad92b70.

--- public/assets/js/datatables-demo.js
+++
@@ -1,7 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function () {
-  $('#dataTable').DataTable({
-    /* No ordering applied by DataTables during initialisation */
-    "order": []
-  });
-});
